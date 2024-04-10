23471263321e493eadc82a8871e2754012002e6a
Hills007 Asad <toorkhan2000@yahoo.com>
2024-04-10 21:34:04 +0000

    implement add ship method to check if maximum no of ships reached and updated

--- run.py
+++ run.py
@@ -27,4 +27,11 @@ class Board:
             return "Miss"
 
     def add_ship(self, x, y):
+        if len(self.ships) >= self.num_ships:
+            print("Error: you cannot add any more ships!")
+            return
+        else:
+            self.ships.append((x, y))
+            if self.type == "player":
+                self.board[x][y] = "@"
         
