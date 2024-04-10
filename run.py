ed841d5e2a3ee99513a8ac5a9cdaabb28dae3ed9
Hills007 Asad <toorkhan2000@yahoo.com>
2024-04-10 21:20:15 +0000

    define board class and size and use init constructor

--- run.py
+++ run.py
@@ -1,2 +1,8 @@
 from random import randint
 scores = {"computer": 0, "player": 0}
+
+
+class Board:
+    def __init__(self, size, num_ships, name, board_type):
+    self.size = size
+    self.board = [["." for _ in range(size)] for _ in range(size)]
