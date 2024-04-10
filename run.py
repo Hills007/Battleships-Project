3ffdbb915cc435b14fff3fabe634f253d85b411c
Hills007 Asad <toorkhan2000@yahoo.com>
2024-04-10 21:23:52 +0000

    declare no of ships, name of board and a list to store guesses

--- run.py
+++ run.py
@@ -4,5 +4,11 @@ scores = {"computer": 0, "player": 0}
 
 class Board:
     def __init__(self, size, num_ships, name, board_type):
-    self.size = size
-    self.board = [["." for _ in range(size)] for _ in range(size)]
+        self.size = size
+        self.board = [["." for _ in range(size)] for _ in range(size)]
+        self.board = [["." for _ in range(size)] for _ in range(size)]
+        self.num_ships = num_ships
+        self.name = name
+        self.type = board_type
+        self.guesses = []
+        self.ships = []
